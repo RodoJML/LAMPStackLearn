01116d580a85fea7ad5c4622b534fe21611e68b0
RodoJML <rodojml@me.com>
2023-09-13 21:02:17 -0400

    php practice

---
+++ 01_output.php
@@ -0,0 +1,17 @@
+<?php
+    // Theres no need to close the tag if the file contains only PHP
+    // otherwise close the tag.
+
+    // ";" is required at the end of each statement
+
+    // echo: is used to output strings, numbers, html, etc.
+    echo "Hello World";
+
+    // print: is like echo but it can only output one string
+    print 123;
+
+    // print_r(): single values and arrays
+    print_r([1,2,3,4]);
+
+    
+?>
