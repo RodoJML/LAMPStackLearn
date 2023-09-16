c165f6ed5db5053e96b6f82c0d27a181aa623f06
RodoJML <rodojml@me.com>
2023-09-16 13:27:15 -0400

    loops

--- 05_loops.php
+++ 05_loops.php
@@ -16,6 +16,17 @@ do{
     $x++;
 } while($x <= 10);
 
+// For each loop, syntax
+$posts = ['Post 1', 'Post 2', 'Post 3', 'Post 4'];
+
+    // Function count() returns the number of elements in an array
+    for($x = 0; $x < count($posts); $x++){
+        echo $posts[$x] . '<br>';
+    }
 
 
+    // A simpler and alternative way to do this is using the foreach loop
+    foreach($posts as $post){
+        echo $post . '<br>';
+    }
 ?>
