5b3feb270f37720803f53a4ce99251078d1d9a08
RodoJML <rodojml@me.com>
2023-09-16 13:31:42 -0400

    loops

--- 05_loops.php
+++ 05_loops.php
@@ -29,4 +29,36 @@ $posts = ['Post 1', 'Post 2', 'Post 3', 'Post 4'];
     foreach($posts as $post){
         echo $post . '<br>';
     }
+
+    // If you want to access the index as well
+    foreach($posts as $index => $post){
+        echo $index . ' - ' . $post . '<br>';
+    }
+
+    // You can also apply this over associative arrays
+    $person = [
+        'name' => 'John',
+        'surname' => 'Doe',
+        'age' => 35,
+        'hobbies' => ['Tennis', 'Video Games']
+    ];
+
+    foreach($person as $key => $value){
+        echo "$key - $value <br>";
+    }
+
+    // You can also apply this over multidimensional arrays
+    $cars = [
+        ['Honda', 20, 10],
+        ['Toyota', 30, 20],
+        ['Ford', 23, 12]
+    ];
+
+    foreach($cars as $car){
+        foreach($car as $detail){
+            echo $detail . ' ';
+        }
+        echo '<br>';
+    }
+
 ?>
