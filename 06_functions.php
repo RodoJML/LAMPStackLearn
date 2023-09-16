26f808e5bc412ecfa7c3c0cdcf091b7d63f65060
RodoJML <rodojml@me.com>
2023-09-16 14:54:34 -0400

    learning array functions

--- 06_functions.php
+++ 06_functions.php
@@ -17,4 +17,23 @@
 
     resgisterEmaiL("myemail.com");
 
+
+    function sum($n1, $n2){
+        return $n1 + $n2;
+    }
+
+    $number = sum(10, 5);
+
+    echo $number;
+
+
+    // Arrow functions 
+    // Introduced in PHP 7.4
+    // They are a more concise way of writing functions 
+    // They are anonymous functions (they dont have a name) 
+    // They are useful for callbacks and closures 
+
+    $multiply = fn($n1, $n2) => $n1 * $n2;
+    echo $multiply(10, 5);
+
 ?>
---
+++ 07_array_functions.php
@@ -0,0 +1,42 @@
+<?php
+
+    // Array functions
+    $fruits = ['Apple', 'Orange', 'Pear'];
+
+    // Get Length
+    echo count($fruits); // 3
+
+    // Search for value
+    echo in_array('Apple', $fruits); // 1
+
+    // Add to the end of the array, 2 options: 
+    $fruits[] = 'Banana';
+    array_push($fruits, 'Grapes');
+
+    // Add to the beginning of the array
+    array_unshift($fruits, 'Mango');
+
+    // Remove from the array
+    array_pop($fruits); // Removes the last element
+    array_shift($fruits); // Removes the first element
+    unset($fruits[1]); // Removes the element indicated, on this example at index 1, and the index itself is also gone. 
+
+    // Split array into chunks
+    $chunkedAr = array_chunk($fruits, 2); // 2 is the size of each chunk
+    print_r($chunkedAr);
+
+    // Concatenate arrays
+    $arr1 = [1,2,3];
+    $arr2 = [4,5,6];
+    $arr3 = array_merge($arr1, $arr2);
+    $arr4 = [...$arr1, ...$arr2]; // This is the new way to do it in PHP 7.4
+
+        // You can also merge associative arrays, 
+        // so that one array is the keys and the other is the values
+
+        $arr5 = ['a' => 1, 'b' => 2];
+        $arr6 = ['c' => 3, 'd' => 4];
+        $arr7 = array_merge($arr5, $arr6);
+        print_r($arr7); // ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4]
+    $
+?>
