0f3fd328dd6641b74221c4714f3e1d4ea22d75a1
RodoJML <rodojml@me.com>
2023-09-16 14:58:31 -0400

    learning php

--- 07_array_functions.php
+++ 07_array_functions.php
@@ -39,3 +39,5 @@
         $arr7 = array_merge($arr5, $arr6);
         print_r($arr7); // ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4]
         // If you want to preserve the keys, use the + operator
+
+?>
