ad8a137f936cf14b819d0b83520c095823c61933
RodoJML <rodojml@me.com>
2023-09-16 13:13:46 -0400

    Learning php basics

---
+++ 05_loops.php
@@ -0,0 +1,21 @@
+<?php
+
+for($x = 0 ; $x <=10: $x++){
+    echo "The number is: $x <br>";
+}
+
+$x = 1;
+
+While($x <= 10){
+    echo "The number is: $x <br>";
+    $x++;
+}
+
+do{
+    echo "The number is: $x <br>";
+    $x++;
+} while($x <= 10);
+
+
+
+?>
