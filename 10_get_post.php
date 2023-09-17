1479347e946eb57bf382b3feecbb364a3df8d820
RodoJML <rodojml@me.com>
2023-09-16 22:02:17 -0400

    GET POST testing

--- 10_get_post.php
+++ 10_get_post.php
@@ -6,3 +6,18 @@
 
 <a href="<?php echo $_SERVER['PHP_SELF'];?>?name=John&age=36">Click here</a>
 
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    
+    <div>
+        <label for="name">Name: </label>
+        <input type="text" name="name">
+    </div>
+
+    <div>
+        <label for=age> Age: </label>
+        <inout type="text" name="age">
+    </div>
+
+    <input type="submit" value="Submit" name="submit">
+
+</form>
